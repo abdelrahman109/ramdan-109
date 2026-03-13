53139e2c7f6634f022a98799d6e4e778c9e4e680
abdelrahman ashraf <75756081+abdelrahman109@users.noreply.github.com>
2026-03-14 00:42:20 +0200

    Update constants.py

--- app/constants.py
+++ app/constants.py
@@ -2,8 +2,8 @@
 TICKET_FULL = "full_package"  # حضور
 TICKET_CONTRIBUTION = "contribution"  # مساهمة
 
-# الأسعار الجديدة
-PRICE_BASE_ATTENDANCE = 150  # حضور الحفل (دخول فقط)
+# الأسعار
+PRICE_BASE_ATTENDANCE = 150  # مساهمة وجبة أسر الشهداء
 PRICE_EXTRA_MEAL = 265  # وجبة إضافية لكل فرد
 PRICE_PIN_MEDAL = 150  # بروش + ميدالية
 
